966b2153630620cbcb009e2a2d09add232ebbc17
s3r442 <s3rzhik23@yandex.ru>
2025-07-20 04:41:18 +0300

    more

---
+++ #1/str,int,float.py
@@ -0,0 +1,27 @@
+print("Melons"+'Melons')
+#str записывается single или double quotes
+print("Your's melons")
+print('beauty "melons"')
+#если используем внутри str single или double quotes ограждаем конструкцию оставшимися
+print('Your\'s beauty "melons"')
+#если внутри str используем оба вида quotes, то один из видов выделяем знаком \ и им же ограждаем конструкцию
+print('melon'+'melon')
+#несколько str в одном statement можно записывать с помощью знака +
+print(10*'melon')
+#str можно умножать *
+print('melon\nmelon')
+#\n - new line
+print(r'melon\nmelon')
+#(r'.....') - raw string - делает всю строку литеральной
+print(2*3)
+#multiplication
+print(2**3)
+#degree
+print(20/3)
+#division (float)
+print(20//3)
+#floor division (int)
+print((8-3)*4)
+#bracket - дает приоритет в выражениях
+print(3%2)
+#% - возвращает остаток после division
---
+++ #1/veriables.py
@@ -0,0 +1,20 @@
+x=2
+y=4
+z=y+x
+print(z)
+#variable - переменная, variable name - "x" \ variable value - "2"
+x=9
+z=y+x
+print(z)
+#при написании кода variable value можно изменять
+name = 'melone'
+#variable может хранить в себе любой вид данный (data types: str, int, float, bool)
+print(name)
+print(name[2])
+print(name[-2])
+print(name[2:])
+print(name[:4])
+print(name[2:4])
+print("Your's a"+name[2:])
+#str - immuteble value - в строке каждому элементу присвоен индекс 0,1,2,3,... - мы \n
+# можем вызывать элементы по отдельности, но не можем их менять
