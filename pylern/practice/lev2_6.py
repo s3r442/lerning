dcf6c4e8a44c9ce733ee050017353ddec01337f6
s3r442 <s3rzhik23@yandex.ru>
2025-09-03 14:53:50 +0300

    d

--- pylern/practice/lev2_6.py
+++ pylern/practice/lev2_6.py
@@ -32,12 +32,34 @@
 
     # 3.  Дан некоторый список с числами Слейте пары элементов вместе: 12, 34, 56]
 
-nums_lst_1 = [1, 2, 3, 4, 5, 6]
+# nums_lst_1 = [1, 2, 3, 4, 5, 6]
 
-nums_lst_2 = []
+# nums_lst_2 = []
 
-for i in range(0, len(nums_lst_1), 2):
-    comb = nums_lst_1[i]*10 + nums_lst_1[i+1]
-    nums_lst_2.append(comb)
+# for i in range(0, len(nums_lst_1), 2):
+#     comb = nums_lst_1[i]*10 + nums_lst_1[i+1]
+#     nums_lst_2.append(comb)
 
-print(nums_lst_2)
+# print(nums_lst_2)
+
+
+
+
+    # 4. Дана некоторая строка Сделайте заглавным первый символ каждого второго слова в этой строке
+
+# lower_str = 'aaa bbb ccc eee fff'
+
+# up_str = ''
+
+# for counter_1, char in enumerate(lower_str.split(' '),1):
+#     if counter_1%2 == 0:
+#         for counter_2, item in enumerate(char):
+#             if counter_2 == 0:
+#                 up_str+= item[0].upper()
+#             else:
+#                 up_str+=item.lower()
+#         up_str+= ' '
+#     else:
+#         up_str+=char.lower() + ' '
+
+# print(up_str)
